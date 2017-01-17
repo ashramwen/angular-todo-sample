ec8169b09f4ab55a1547ae7605d9635509b58c34
augustine <ashramwen@gmail.com>
2017-01-17 09:50:09 +0900

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: LoginComponent
   }
 ];
 
