5babe00ec2df25ca695aba168fc5bd04e37e971e
kishimotokii <hideaki.kishimoto@kii.com>
2017-01-19 12:40:35 +0900

    Updated the script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,56 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
-import { Push } from '@ionic/cloud-angular';
+import { Component, ViewChild } from '@angular/core';
 
-import { TabsPage } from '../pages/tabs/tabs';
-declare function require(string): any;
-var kii = require('kii-cloud-sdk').create();
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { StatusBar } from 'ionic-native';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ListPage } from '../pages/list/list';
+import {Push} from 'ionic-native';
+//import {Kii, KiiSite, KiiUser} from 'kii-cloud-sdk';
+import * as kii from 'kii-cloud-sdk'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  @ViewChild(Nav) nav: Nav;
 
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = HelloIonicPage;
+  pages: Array<{title: string, component: any}>;
 
+  constructor(
+    public platform: Platform,
+    public menu: MenuController
+  ) {
+    this.initializeApp();
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+    // set our app's pages
+    this.pages = [
+      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'My First List', component: ListPage }
+    ];
+  }
 
-
-      console.log("ready");
-      // kii = window['kii'].create();  // Kii push plug-in
-      kii.Kii.initializeWithSite("58d9081d", "2932582ebd12d1ce69fca7f3d77adb09", kii.KiiSite.JP);
-
-      console.log("Push.init");
-      let push = Push.init({
+  initializeApp() {
+    this.platform.ready().then(() => {
+      StatusBar.styleDefault();
+      var push = Push.init({
         android: {
           senderID: "603734069394"
         },
         ios: {
           alert: "true",
-          badge: false,
-          sound: "true"
+          badge: true,
+          sound: 'false'
         },
         windows: {}
       });
-
-      console.log("push.on(registration)");
       push.on('registration', (data) => {
         console.log("registered");
+        console.log(data.registrationId);
+        kii.Kii.initializeWithSite("58d9081d", "2932582ebd12d1ce69fca7f3d77adb09", kii.KiiSite.JP);
         kii.KiiUser.authenticate("aaaa", "bbbb", {
           success: function (theUser) {
             console.log("user authenticated");
@@ -59,15 +69,19 @@ export class MyApp {
         });
       });
       push.on('notification', (data) => {
-        console.log(data.message);
+        console.log(data);
+        alert("Hi, Am a push notification");
       });
       push.on('error', (e) => {
         console.log(e.message);
       });
-
-      StatusBar.styleDefault();
-      Splashscreen.hide();
     });
   }
-}
 
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
